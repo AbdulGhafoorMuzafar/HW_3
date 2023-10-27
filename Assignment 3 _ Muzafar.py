d84e9a1183a604b530e41dc288a30f8c9982e739
Abdul Ghafoor Muzafar <148848423+AbdulGhafoorMuzafar@users.noreply.github.com>
2023-10-27 10:18:42 -0700

    Update Assignment 3 _ Muzafar.py

--- Assignment 3 _ Muzafar.py
+++ Assignment 3 _ Muzafar.py
@@ -8,22 +8,27 @@ print("5 : factorial")
 print("6 : sqrt")
 operation = int(input())
 
-number = int (input("Please Enter Your Number :"))
 if (operation ==1) :
+    number = float (input("Please Enter Your Number :"))
     print("Sin of your number is : ") 
     print(math.sin(number))
 elif (operation == 2) :
+    number = float (input("Please Enter Your Number :"))
     print("Cos Of Your Number Is :")
     print(math.cos(number))
 elif(operation == 3) :
+    number = float (input("Please Enter Your Number :"))
     print("Tan Of Your Number Is :")
     print(math.tan(number))
 elif(operation == 4) :
+    number = float (input("Please Enter Your Number :"))
     print("Cot Of Your Number Is :")
     print(1/math.tan(number))
 elif(operation == 5) :
+    number = int (input("Please Enter Your Number :"))
     print("Factorial Of Your Number Is :")
     print(math.factorial(number))
 elif(operation == 6) :
+    number = int (input("Please Enter Your Number :"))
     print("sqrt Of Your Number Is :")
     print(math.sqrt(number))
